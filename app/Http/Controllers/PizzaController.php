f57be161eb0e2950e4904ff2968bcb12fc6b421b
Christoffer Ivano <christoffer.ivano@gmail.com>
2021-02-17 20:50:27 +0700

    4th commit

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -7,6 +7,11 @@ use App\Pizza;
 
 class PizzaController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
     public function index()
     {
         // $pizzas = Pizza::all();
@@ -37,9 +42,18 @@ class PizzaController extends Controller
         $pizza->name = request('name');
         $pizza->type = request('type');
         $pizza->base = request('base');
+        $pizza->toppings = request('toppings');
 
         $pizza->save();
 
         return redirect('/')->with('mssg', 'Thanks for your order');
+    }
+
+    public function destroy($id)
+    {
+        $pizza = Pizza::findOrFail($id);
+        $pizza->delete();
+
+        return redirect('/pizzas');
     }
 }
--- database/migrations/2021_02_17_134608_create_kebabs_table.php
+++ database/migrations/2021_02_17_134608_create_kebabs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePizzaTable extends Migration
+class CreateKebabsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreatePizzaTable extends Migration
      */
     public function up()
     {
-        Schema::create('pizzas', function (Blueprint $table) {
+        Schema::create('kebabs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
-            $table->string('type');
-            $table->string('base');
-            $table->string('name');
-            $table->json('toppings');
         });
     }
 
@@ -30,6 +26,6 @@ class CreatePizzaTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pizza');
+        Schema::dropIfExists('kebabs');
     }
 }
